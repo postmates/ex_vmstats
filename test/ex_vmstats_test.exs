ec72b738346d5973b0e7f2eb9331b89a113e284c
Sean Cribbs <sean.cribbs@postmates.com>
2018-12-10 14:24:52 -0800

    Add fixes for OTP 21
    - :error_logger is no longer a mandatory process
    - Dirty CPU schedulers get instrumented by :scheduler_wall_time
    - mix format

--- test/ex_vmstats_test.exs
+++ test/ex_vmstats_test.exs
@@ -9,7 +9,6 @@ defmodule ExVmstatsTest do
     ~r/messages_in_queues/,
     ~r/modules/,
     ~r/run_queue/,
-    ~r/error_logger_queue_len/,
     ~r/io.bytes_in/,
     ~r/io.bytes_out/,
     ~r/gc.count/,
@@ -21,6 +20,12 @@ defmodule ExVmstatsTest do
     ~r/memory.binary/,
     ~r/memory.ets/
   ]
+
+  @is_before_otp_21 :erlang.list_to_integer(:erlang.system_info(:otp_release)) < 21
+
+  if @is_before_otp_21 do
+    @metric_regexes [~r/error_logger_queue_len/ | @metric_regexes]
+  end
 
   @number_of_metrics length(@metric_regexes)
 
@@ -34,17 +39,19 @@ defmodule ExVmstatsTest do
   end
 
   test "no stats sent before timeout" do
-    capture = capture_log fn ->
-      run_and_terminate_server(400)
-    end
+    capture =
+      capture_log(fn ->
+        run_and_terminate_server(400)
+      end)
 
     assert capture == ""
   end
 
   test "single set of stats is sent after timeout" do
-    capture = capture_log fn ->
-      run_and_terminate_server(600)
-    end
+    capture =
+      capture_log(fn ->
+        run_and_terminate_server(600)
+      end)
 
     for regex <- @metric_regexes do
       assert match_count(regex, capture) == 1
@@ -54,9 +61,10 @@ defmodule ExVmstatsTest do
   end
 
   test "two sets of stats are sent after two timeouts" do
-    capture = capture_log fn ->
-      run_and_terminate_server(1200)
-    end
+    capture =
+      capture_log(fn ->
+        run_and_terminate_server(1200)
+      end)
 
     for regex <- @metric_regexes do
       assert match_count(regex, capture) == 2
@@ -68,11 +76,18 @@ defmodule ExVmstatsTest do
   test "use_histogram" do
     Application.put_env(:ex_vmstats, :use_histogram, true)
 
-    capture = capture_log fn ->
-      run_and_terminate_server(600)
+    capture =
+      capture_log(fn ->
+        run_and_terminate_server(600)
+      end)
+
+    if @is_before_otp_21 do
+      assert match_count(~r/histogram/, capture) == 11
+    else
+      # NB: No error_logger_queue_len
+      assert match_count(~r/histogram/, capture) == 10
     end
 
-    assert match_count(~r/histogram/, capture) == 11
     assert match_count(~r/gauge/, capture) == 0
 
     Application.put_env(:ex_vmstats, :use_histogram, false)
@@ -81,14 +96,23 @@ defmodule ExVmstatsTest do
   test "sched_time enabled" do
     Application.put_env(:ex_vmstats, :sched_time, true)
 
-    capture = capture_log fn ->
-      run_and_terminate_server(600)
-    end
+    capture =
+      capture_log(fn ->
+        run_and_terminate_server(600)
+      end)
 
-    scheduler_metric_count = :erlang.system_info(:schedulers) * 2
+    scheduler_metric_count =
+      2 *
+        if @is_before_otp_21 do
+          :erlang.system_info(:schedulers)
+        else
+          :erlang.system_info(:schedulers) + :erlang.system_info(:dirty_cpu_schedulers)
+        end
 
     assert match_count(~r/timer/, capture) == scheduler_metric_count
-    assert match_count(~r/scheduler_wall_time.(\d+)(.active|.total)/, capture) == scheduler_metric_count
+
+    assert match_count(~r/scheduler_wall_time.(\d+)(.active|.total)/, capture) ==
+             scheduler_metric_count
 
     Application.put_env(:ex_vmstats, :sched_time, false)
   end
